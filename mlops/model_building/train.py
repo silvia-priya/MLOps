77895cf0dba6ab538b3d3dd41108785dcec67759
Silvia Priyadharshini <67099867+silvia-priya@users.noreply.github.com>
2025-12-11 15:49:12 +0530

    Update train.py

--- mlops/model_building/train.py
+++ mlops/model_building/train.py
@@ -27,6 +27,7 @@ Xtest = pd.read_csv(Xtest_path)
 ytrain = pd.read_csv(ytrain_path)
 ytest = pd.read_csv(ytest_path)
 
+print("ytest data",ytest)
 
 # List of numerical features in the dataset
 numeric_features = [
